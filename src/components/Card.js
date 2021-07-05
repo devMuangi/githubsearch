b00ce041377b812873981660701161a91c913e37
devMuangi <devmuangi@gmail.com>
2021-07-05 17:49:39 +0300

    some tweaks

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 
 const Card = () => {
   const { githubUser } = React.useContext(GithubContext);
-  const {avatar_url,html_url,name,company,blog,bio,location,twitter_username,} = githubUser;
+  const {avatar_url,html_url,name,company,blog,bio,location} = githubUser;
 
   return (
     <Wrapper>
